4a717a105d5a5b560a5248562a4199e20751d6bb
twolak88 <twolak88@gmail.com>
2021-03-01 18:01:57 +0100

    Redirecting Requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import classes from './Blog.module.css';
 import NewPost from './NewPost/NewPost';
 import Posts from './Posts/Posts';
@@ -40,6 +40,12 @@ class Blog extends Component {
           <Route 
             path="/posts" 
             component={Posts}/>
+          <Redirect 
+            from="/" 
+            to="/posts" />
+          {/* <Route 
+            path="/" 
+            component={Posts}/> */}
         </Switch>
       </div>
     );
