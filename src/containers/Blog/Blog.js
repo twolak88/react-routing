afa897ef9f286f1faaaac7d64d970fd6dfc462f6
twolak88 <twolak88@gmail.com>
2021-03-01 20:04:15 +0100

    Lazy Loading with React Suspense (16.6)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,40 @@
-import { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
-import AsyncComponent from '../../hoc/asyncComponent';
+import React, { Component, Suspense } from 'react';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+// import AsyncComponent from '../../hoc/asyncComponent';
 import classes from './Blog.module.css';
 import Posts from './Posts/Posts';
+import Welcome from './Welcome/Welcome';
 // import NewPost from './NewPost/NewPost';
 
-const AsyncNewPost = AsyncComponent(() => {
-  return import('./NewPost/NewPost');
-});
+// const AsyncNewPost = AsyncComponent(() => {
+//   return import('./NewPost/NewPost');
+// });
+
+const AsyncNewPost = React.lazy(() => import('./NewPost/NewPost'));
 
 class Blog extends Component {
   state = {
-    auth: true
+    auth: true,
+    // show: false
   }
+
+  // modeHandler = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       show: !prevState.show
+  //     };
+  //   })
+  // }
 
   render() {
     return (
+      // <React.Fragment>
+      //   <button onClick={this.modeHandler}>Toggle mode</button>
+      //   {this.state.show ? <Suspense fallback={<div>Loading...</div>}>
+      //             <AsyncNewPost/>
+      //           </Suspense> : null}
+      // </React.Fragment>
+
       <div className={classes.Blog}>
         <header>
           <nav>
@@ -41,20 +60,25 @@ class Blog extends Component {
         {/* <Route path="/" exact render={() => <h1>Home</h1>} />
         <Route path="/" render={() => <h1>Home2</h1>} /> */}
         <Switch>
+          <Route 
+            path='/welcome'
+            component={Welcome}/>
           {
             this.state.auth ? 
             <Route 
               path="/new-post" 
-              component={AsyncNewPost}/> : null
+              render={() => <Suspense fallback={<div>Loading...</div>}>
+                <AsyncNewPost/>
+              </Suspense>}/> : null
           }
           <Route 
             path="/posts" 
             component={Posts}/>
-          <Route 
-            render={() => <h1>Not found!</h1>}/>
-          {/* <Redirect 
+          {/* <Route 
+            render={() => <h1>Not found!</h1>}/> */}
+          <Redirect 
             from="/" 
-            to="/posts" /> */}
+            to="/welcome" />
           {/* <Route 
             path="/" 
             component={Posts}/> */}
